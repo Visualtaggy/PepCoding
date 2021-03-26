52b131b313d2af8dad65485947dc8e16141beb0f
VisualTaggy <visualtaggy@gmail.com>
2021-03-26 13:42:01 +0530

    + completing module

--- 2.RecursionAndBacktracking/RecursionOnTheWayUp/PrintPermutationOfficial.java
+++ 2.RecursionAndBacktracking/RecursionOnTheWayUp/PrintPermutationOfficial.java
@@ -3,8 +3,30 @@
 // Use sample input and output to take idea about permutations.
 
 // Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+import java.util.*;
+
 public class PrintPermutationOfficial {
     public static void main(String[] args) {
-        
+        Scanner scn = new Scanner(System.in);
+        String ques = scn.next();
+        printPermutations(ques, "");
+        scn.close();
+    }
+
+    public static void printPermutations(String str, String ans) {
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char firstChar = str.charAt(i);
+            String leftString = str.substring(0, i);
+            String rightString = str.substring(i + 1);
+            String remainingString = leftString + rightString;
+
+            printPermutations(remainingString, ans + firstChar);
+        }
+
     }
 }
