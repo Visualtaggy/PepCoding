7f5b4c4974e8b7d531f3ec5e9a5ebce71209decd
VisualTaggy <visualtaggy@gmail.com>
2021-03-28 20:55:52 +0530

    +new question

---
+++ 2.RecursionAndBacktracking/Backtracking/KnightsTourOfficial.java
@@ -0,0 +1,36 @@
+import java.util.*;
+// 1. You are given a number n, the size of a chess board.
+// 2. You are given a row and a column, as a starting point for a knight piece.
+// 3. You are required to generate the all moves of a knight starting in (row, col) such that knight visits 
+//      all cells of the board exactly once.
+// 4. Complete the body of printKnightsTour function - without changing signature - to calculate and 
+//      print all configurations of the chess board representing the route
+//      of knight through the chess board. Use sample input and output to get more idea.
+
+// Note -> When moving from (r, c) to the possible 8 options give first precedence to (r - 2, c + 1) and 
+//                move in clockwise manner to
+//                explore other options.
+// Note -> The online judge can't force you to write the function recursively but that is what the spirit of 
+//                question is. Write recursive and not iterative logic. The purpose of the question is to aid 
+//                learning recursion and not test you.
+
+public class KnightsTourOfficial {
+    public static void main(String[] args) {
+
+    }
+
+    public static void printKnightsTour(int[][] chess, String qsf, int row) {
+
+    }
+
+    public static void displayBoard(int[][] chess) {
+        for (int i = 0; i < chess.length; i++) {
+            for (int j = 0; j < chess[0].length; j++) {
+                System.out.print(chess[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+    }
+}
