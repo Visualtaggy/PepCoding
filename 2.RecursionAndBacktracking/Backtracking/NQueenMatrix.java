79e7308a1f17797b80817ea9e2182f0cd9147d19
VisualTaggy <visualtaggy@gmail.com>
2021-03-28 19:44:49 +0530

    + new question

---
+++ 2.RecursionAndBacktracking/Backtracking/NQueenMatrix.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+// 1. You are given a number n, the size of a chess board.
+// 2. You are required to place n number of queens in the n * n cells of board such that no queen can kill another.
+// Note - Queens kill at distance in all 8 directions
+// 3. Complete the body of printNQueens function - without changing signature - to calculate and print all safe configurations of n-queens. Use sample input and output to get more idea.
+
+// Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+
+public class NQueenMatrix {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+
+        int[][] arr = new int[n][m];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        printNQueens(arr, "", 0);
+        scn.close();
+    }
+
+    public static void printNQueens(int[][] chess, String qsf, int row) {
+
+    }
+}
