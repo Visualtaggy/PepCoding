51a7c7d8df35a1bfaa68cdbfc5604444d573971b
VisualTaggy <visualtaggy@gmail.com>
2021-04-02 22:09:15 +0530

    +solved

--- 3.BasicDataStructures/StacksAndQues/BalancedBrackets.java
+++ 3.BasicDataStructures/StacksAndQues/BalancedBrackets.java
@@ -7,12 +7,67 @@
 // [(a + b) + {(c + d) * (e / f)]} -> false
 // [(a + b) + {(c + d) * (e / f)} -> false
 // ([(a + b) + {(c + d) * (e / f)}] -> false
-import java.io.*;
+
 import java.util.*;
 
 public class BalancedBrackets {
 
+    public static boolean edgeCaseDetector(Stack<Character> st, char ref) {
+
+        if (st.size() == 0) {
+            return false;
+        } else if (st.peek() != ref) {
+            return false;
+        } else {
+            st.pop();
+            return true;
+        }
+    }
+
     public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+
+        String str = scn.nextLine();
+        scn.close();
+
+        Stack<Character> st = new Stack<>();
+
+        // Looping over elements of the string
+        for (int i = 0; i < str.length(); i++) {
+
+            char current = str.charAt(i);
+            if (current == '(' || current == '{' || current == '[') {
+                st.push(current);
+            } else if (current == ')') {
+                boolean val = edgeCaseDetector(st, '(');
+                if (val == false) {
+                    System.out.println(val);
+                    return;
+                }
+
+            } else if (current == '}') {
+                boolean val = edgeCaseDetector(st, '{');
+                if (val == false) {
+                    System.out.println(val);
+                    return;
+                }
+
+            } else if (current == ']') {
+                boolean val = edgeCaseDetector(st, '[');
+                if (val == false) {
+                    System.out.println(val);
+                    return;
+                }
+
+            }
+
+        }
+
+        if (st.size() == 0) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
 
     }
 
