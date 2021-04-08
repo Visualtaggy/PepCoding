a1fe3ef30822ee2c002ce7dc9f811f7e493cf671
VisualTaggy <visualtaggy@gmail.com>
2021-04-08 22:53:33 +0530

    +solved

--- 3.BasicDataStructures/LinkedList/RemoveFirstInLinkedList.java
+++ 3.BasicDataStructures/LinkedList/RemoveFirstInLinkedList.java
@@ -51,7 +51,15 @@ public class RemoveFirstInLinkedList {
 
         public void removeFirst() {
             // write your code here
-            
+            if (size == 0) {
+                System.out.println("List is empty");
+            } else if (size == 1) {
+                head = tail = null;
+                size = 0;
+            } else {
+                head = head.next;
+                size--;
+            }
         }
     }
 
