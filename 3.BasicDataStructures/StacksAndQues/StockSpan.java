df863341fb0c25659555d6b9704581dca75aa1e2
VisualTaggy <visualtaggy@gmail.com>
2021-04-08 22:33:20 +0530

    +solved

--- 3.BasicDataStructures/StacksAndQues/StockSpan.java
+++ 3.BasicDataStructures/StacksAndQues/StockSpan.java
@@ -1,8 +1,7 @@
 // 1. You are given a number n, representing the size of array a.
 // 2. You are given n numbers, representing the prices of a share on n days.
 // 3. You are required to find the stock span for n days.
-// 4. Stock span is defined as the number of days passed between the current day
-// and the first day before today when price was higher than today.
+// 4. Stock span is defined as the number of days passed between the current day and the first day before today when price was higher than today.
 
 // e.g.
 // for the array [2 5 9 3 1 12 6 8 7]
@@ -15,3 +14,52 @@
 // span for 6 is 1
 // span for 8 is 2
 // span for 7 is 1
+import java.io.*;
+import java.util.*;
+
+public class StockSpan {
+    public static void display(int[] a) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int val : a) {
+            sb.append(val + "\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] span = solve(a);
+        display(span);
+    }
+
+    public static int[] solve(int[] arr) {
+        int[] answer = new int[arr.length];
+        answer[0] = 1;
+        Stack<Integer> st = new Stack<>();
+        st.push(0);
+        for (int i = 1; i < arr.length; i++) {
+
+            while (st.size() > 0 && arr[i] >= arr[st.peek()]) {
+                st.pop();
+            }
+            if (st.size() == 0) {
+                answer[i] = i + 1;
+            } else {
+                answer[i] = i - st.peek();
+            }
+
+            st.push(i);
+        }
+        // solve
+        return answer;
+    }
+
+}
