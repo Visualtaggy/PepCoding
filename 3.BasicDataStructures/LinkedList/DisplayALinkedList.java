a71cde188e6927bdc252b7adfaf3843aa5a23b81
VisualTaggy <visualtaggy@gmail.com>
2021-04-08 22:48:45 +0530

    +size

--- 3.BasicDataStructures/LinkedList/DisplayALinkedList.java
+++ 3.BasicDataStructures/LinkedList/DisplayALinkedList.java
@@ -38,10 +38,17 @@ public class DisplayALinkedList {
 
         public int size() {
             // write code here
+            return size;
         }
 
         public void display() {
             // write code here
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+            System.out.println();
         }
     }
 
