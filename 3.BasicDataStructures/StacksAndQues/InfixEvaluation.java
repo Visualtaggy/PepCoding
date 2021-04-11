1d0f6ac88ad95a8445ee1610cf605abf7683a0dd
VisualTaggy <visualtaggy@gmail.com>
2021-04-11 19:37:53 +0530

    +quesiton

---
+++ 3.BasicDataStructures/StacksAndQues/InfixEvaluation.java
@@ -0,0 +1,13 @@
+
+// 1. You are given an infix expression.
+// 2. You are required to evaluate and print it's value.
+import java.io.*;
+import java.util.*;
+
+public class InfixEvaluation {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String exp = br.readLine();
+        // code
+    }
+}
