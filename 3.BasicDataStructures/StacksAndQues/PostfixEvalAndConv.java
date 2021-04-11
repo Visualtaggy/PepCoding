8d97f82d5af00f22a7a97263031228fde83b5321
VisualTaggy <visualtaggy@gmail.com>
2021-04-11 11:47:05 +0530

    +solved

--- 3.BasicDataStructures/StacksAndQues/PostfixEvalAndConv.java
+++ 3.BasicDataStructures/StacksAndQues/PostfixEvalAndConv.java
@@ -4,6 +4,59 @@
 // 4. You are required to convert it to prefix and print it.
 
 // Note -> Use brackets in infix expression for indicating precedence. Check sample input output for more details.
-public class PostfixEvalAndConv {
+import java.io.*;
+import java.util.*;
 
+public class PostfixEvalAndConv {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String exp = br.readLine();
+
+        // code
+        Stack<Integer> vs = new Stack<>();
+        Stack<String> is = new Stack<>();
+        Stack<String> ps = new Stack<>();
+
+        // looping over string
+        for (int i = 0; i < exp.length(); i++) {
+            char ch = exp.charAt(i);
+
+            if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+                int v2 = vs.pop();
+                int v1 = vs.pop();
+                int val = operation(v1, v2, ch);
+                vs.push(val);
+                // is
+                String iv2 = is.pop();
+                String iv1 = is.pop();
+                String ival = "(" + iv1 + ch + iv2 + ")";
+                is.push(ival);
+
+                String pv2 = ps.pop();
+                String pv1 = ps.pop();
+                String pval = ch + pv1 + pv2;
+                ps.push(pval);
+            } else {
+                vs.push(ch - '0');
+                is.push(ch + "");
+                ps.push(ch + "");
+            }
+
+        }
+        System.out.println(vs.pop());
+        System.out.println(is.pop());
+        System.out.println(ps.pop());
+    }
+
+    public static int operation(int v1, int v2, char op) {
+        if (op == '+') {
+            return v1 + v2;
+        } else if (op == '-') {
+            return v1 - v2;
+        } else if (op == '/') {
+            return v1 / v2;
+        } else {
+            return v1 * v2;
+        }
+    }
 }
