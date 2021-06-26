6cf706d5b266fd8e4c9a940bd920adcd47379cb4
VisualTaggy <visualtaggy@gmail.com>
2021-06-26 21:16:07 +0530

    +intro to understand the structure

---
+++ 3.BasicDataStructures/BinaryTree/IntroToGenericTree.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class IntroToGenericTree {
+
+    private class Node {
+        // Storing the data
+        int data;
+        // Stores data of all the children
+        ArrayList<Node> children = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        // This refers to the main root node which is one the top.
+        Node root;
+
+    }
+}
