eff27ac31906d4ccb597e39d045f1f8d1794ff3b
VisualTaggy <visualtaggy@gmail.com>
2021-06-26 23:50:43 +0530

    +question

--- 3.BasicDataStructures/GenericTree/SizeOfGenericTree.java
+++ 3.BasicDataStructures/GenericTree/SizeOfGenericTree.java
@@ -1,3 +1,8 @@
+// 1. You are given a partially written GenericTree class.
+// 2. You are required to complete the body of size function. The function is expected to count the number of nodes in the tree and return it.
+// 3. Input and Output is managed for you.
 public class SizeOfGenericTree {
-    
+    public static void main(String[] args) {
+
+    }
 }
