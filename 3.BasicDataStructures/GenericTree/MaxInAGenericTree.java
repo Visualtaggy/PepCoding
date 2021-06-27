fe2c7763a707e3b3f9850d8220e178dbfcb353b6
VisualTaggy <visualtaggy@gmail.com>
2021-06-27 17:01:34 +0530

    + question

---
+++ 3.BasicDataStructures/GenericTree/MaxInAGenericTree.java
@@ -0,0 +1,9 @@
+// 1. You are given a partially written GenericTree class.
+// 2. You are required to complete the body of max function. The function is expected to find the node with maximum value and return it.
+// 3. Input and Output is managed for you
+
+public class MaxInAGenericTree {
+    public static void main(String[] args) {
+
+    }
+}
