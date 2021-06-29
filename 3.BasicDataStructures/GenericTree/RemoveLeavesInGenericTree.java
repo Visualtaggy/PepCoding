b1c4dddc9632bea1276dfedf12fb3787b1a5be86
VisualTaggy <visualtaggy@gmail.com>
2021-06-29 16:22:45 +0530

    +question

---
+++ 3.BasicDataStructures/GenericTree/RemoveLeavesInGenericTree.java
@@ -0,0 +1,9 @@
+// 1. You are given a partially written GenericTree class.
+// 2. You are required to complete the body of removeLeaves function. The function is expected to remove all leaves from the tree. For more details, check out the question video.
+// 3. Input and Output is managed for you.
+
+public class RemoveLeavesInGenericTree {
+    public static void main(String[] args) {
+
+    }
+}
