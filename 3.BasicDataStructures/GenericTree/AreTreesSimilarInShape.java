f16916436cda5b89db664b14f2a866c5f0864585
VisualTaggy <visualtaggy@gmail.com>
2021-07-01 15:06:32 +0530

    +question

---
+++ 3.BasicDataStructures/GenericTree/AreTreesSimilarInShape.java
@@ -0,0 +1,9 @@
+// 1. You are given a partially written GenericTree class.
+// 2. You are required to complete the body of areSimilar function. The function is expected to check if the two trees passed to it are similar in shape or not.
+// 3. Input and Output is managed for you. 
+
+public class AreTreesSimilarInShape {
+    public static void main(String[] args) {
+
+    }
+}
