5b0d94e4cc7029929c699ea4dd4d5d9d10416624
VisualTaggy <visualtaggy@gmail.com>
2021-07-19 19:45:52 +0530

    +question

---
+++ 3.BasicDataStructures/GenericTree/PredecessorAndSuccessorOfAnElement.java
@@ -0,0 +1,9 @@
+// 1. You are given a partially written GenericTree class.
+// 2. You are required to find the preorder predecessor and successor of a given element. Use the "travel and change" strategy explained in the earlier video. The static properties have been declared for you. You can declare more if you want.
+// 3. Input and Output is managed for you.
+
+public class PredecessorAndSuccessorOfAnElement {
+    public static void main(String[] args) {
+
+    }
+}
