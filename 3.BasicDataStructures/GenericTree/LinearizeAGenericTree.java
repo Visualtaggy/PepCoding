4b2062d9266fec8ec0ee6bd8a2b9b5223058d0e7
VisualTaggy <visualtaggy@gmail.com>
2021-08-02 21:21:10 +0530

    + solved leanirze

--- 3.BasicDataStructures/GenericTree/LinearizeAGenericTree.java
+++ 3.BasicDataStructures/GenericTree/LinearizeAGenericTree.java
@@ -150,6 +150,24 @@ public class LinearizeAGenericTree {
 
     public static void linearize(Node node) {
         // write your code here
+        for (Node child : node.children) {
+            linearize(child); // linear
+        }
+
+        while (node.children.size() > 1) {
+            Node lc = node.children.remove(node.children.size() - 1); // extracting last child
+            Node sl = node.children.get(node.children.size() - 1);
+            Node slt = getTail(sl);
+            slt.children.add(lc); // linking
+
+        }
+    }
+
+    private static Node getTail(Node node) {
+        while (node.children.size() == 1) {
+            node = node.children.get(0);
+        }
+        return node;
     }
 
     public static void main(String[] args) throws Exception {
