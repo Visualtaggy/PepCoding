6bc8f7607b7a196b768109e0ddf77100a70c48e7
VisualTaggy <visualtaggy@gmail.com>
2021-08-07 16:50:03 +0530

    +basic structure of binary tree

---
+++ 3.BasicDataStructures/BinaryTree/BinaryTree.java
@@ -0,0 +1,25 @@
+/**
+ * BinaryTree This is the introduction to binary tree how it's declared
+ */
+public class BinaryTree {
+
+    public static class Node {
+        // This is the data inside the node
+        int data;
+
+        // These are the child nodes
+        Node left;
+        Node right;
+
+        // This is the constructor function
+        Node(int data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
