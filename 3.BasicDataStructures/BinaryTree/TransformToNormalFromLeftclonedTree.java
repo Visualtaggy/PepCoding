a275429715c2dd2f3ea0339dbca7752c7034c4a3
VisualTaggy <visualtaggy@gmail.com>
2021-09-06 13:56:41 +0530

    [QUES] Tranform to normal from cloned tree

---
+++ 3.BasicDataStructures/BinaryTree/TransformToNormalFromLeftclonedTree.java
@@ -0,0 +1,11 @@
+// 1. You are given a partially written BinaryTree class.
+// 2. You are required to complete the body of transBackFromLeftClonedTree function. The function is expected to convert a left-cloned tree back to it's original form. The left cloned tree is dicussed in previous question. In a left-clone tree a new node for every node equal in value to it is inserted between itself and it's left child. For clarity check out the question video.
+// 3. Input and Output is managed for you.
+
+
+
+public class TransformToNormalFromLeftclonedTree {
+    public static void main(String[] args) {
+
+    }
+}
