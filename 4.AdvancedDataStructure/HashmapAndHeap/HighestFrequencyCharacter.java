b81831f2be50e5bf24f3c43e02c9b3a8cef1182a
VisualTaggy <visualtaggy@gmail.com>
2021-09-17 00:06:41 +0530

    [Quest]: Highest Freq

---
+++ 4.AdvancedDataStructure/HashmapAndHeap/HighestFrequencyCharacter.java
@@ -0,0 +1,11 @@
+// 1. You are given a string str.
+// 2. You are required to find the character with maximum frequency.
+
+import java.io.*;
+import java.util.*;
+
+public class HighestFrequencyCharacter {
+    public static void main(String[] args) {
+
+    }
+}
