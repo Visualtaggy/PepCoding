50d11972bc9a394dd674742f00c6d77f260f4fb4
VisualTaggy <visualtaggy@gmail.com>
2021-09-18 22:34:12 +0530

    [Soln]: Longest Consecutive Sequence

--- 4.AdvancedDataStructure/HashmapAndHeap/LongestConsecutiveSequenceOfElements.java
+++ 4.AdvancedDataStructure/HashmapAndHeap/LongestConsecutiveSequenceOfElements.java
@@ -4,8 +4,63 @@
 
 // Note -> In case there are two sequences of equal length (and they are also the longest), then print the one for which the starting point of which occurs first in the array.
 
-public class LongestConsecutiveSequenceOfElements {
-    public static void main(String[] args) {
+ import java.io.*;
+import java.util.*;
 
+public class LongestConsecutiveSequenceOfElements{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+    
+    Scanner scn  = new Scanner(System.in);
+    
+    int n = scn.nextInt();
+    
+    int[] arr  = new int[n];
+    
+    for(int i = 0; i < arr.length; i++){
+        arr[i] = scn.nextInt();
     }
+    
+    HashMap<Integer, Boolean> hm = new HashMap<>();
+    
+    for(int val : arr){
+        hm.put(val,true);
+    }
+    
+    for(int val : arr){
+        if(hm.containsKey(val - 1)){
+            hm.put(val,false);
+        }
+    }
+    
+    
+    int msp = 0;
+    int ml = 0;
+    
+    for(int val : arr){
+        
+        if(hm.get(val) == true){
+            
+            int tl = 1;
+            int tsp = val;
+            
+            while(hm.containsKey(tsp + tl)){
+                tl++;
+            }
+            
+            if(tl > ml){
+                msp = tsp;
+                ml = tl;
+            }
+        }
+        
+    }
+    
+    for(int i = 0; i < ml; i++){
+        System.out.println(msp + i);
+    }
+    
+ }
+
 }
