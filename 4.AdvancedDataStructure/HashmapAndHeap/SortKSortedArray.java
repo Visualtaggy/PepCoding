12317e1501ee6fdd6ba157ad2cbea726b7a0f247
VisualTaggy <visualtaggy@gmail.com>
2021-09-19 02:49:05 +0530

    [Soln]: Sort a partially sorted array

--- 4.AdvancedDataStructure/HashmapAndHeap/SortKSortedArray.java
+++ 4.AdvancedDataStructure/HashmapAndHeap/SortKSortedArray.java
@@ -1,3 +1,4 @@
+
 /**
  SortKSortedArray
  1. You are given a number n, representing the size of array a.
@@ -7,9 +8,42 @@
 
 Note -> You can use at-max k extra space and nlogk time complexity.
  */
+import java.io.*;
+import java.util.*;
+
 public class SortKSortedArray {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int k = Integer.parseInt(br.readLine());
+
+        // write your code here
+        PriorityQueue<Integer> que = new PriorityQueue<>();
+
+        // making a funnel of K size
+        for (int i = 0; i <= k; i++) {
+            que.add(arr[i]);
+        }
+
+        for (int i = k + 1; i < arr.length; i++) {
+            System.out.println(que.remove());
+
+            // adding next element to maintain K size of funnel
+            que.add(arr[i]);
+        }
+
+        // check if funnel is empty
+        while (que.size() > 0) {
+            System.out.println(que.remove());
+        }
+
     }
 }
