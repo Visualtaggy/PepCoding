42d5aac107dbb63dc9769b57ee8ad6320f60814d
VisualTaggy <visualtaggy@gmail.com>
2021-09-21 23:12:52 +0530

    [Quest]:Write Hash Map

---
+++ 4.AdvancedDataStructure/HashmapAndHeap/WriteHashmap.java
@@ -0,0 +1,105 @@
+// 1. You are required to complete the code of our Hashmap class. Please watch the question video carefully. The theoretical details of required functionality is explained in detail there. Implement the functions to achieve what is explained in the theoretical discussion in question video.
+// 2. Input and Output is managed for you.
+
+import java.io.*;
+import java.util.*;
+
+public class WriteHashmap  {
+
+  public static class HashMap<K, V> {
+    private class HMNode {
+      K key;
+      V value;
+
+      HMNode(K key, V value) {
+        this.key = key;
+        this.value = value;
+      }
+    }
+
+    private int size; // n
+    private LinkedList<HMNode>[] buckets; // N = buckets.length
+
+    public HashMap() {
+      initbuckets(4);
+      size = 0;
+    }
+
+    private void initbuckets(int N) {
+      buckets = new LinkedList[N];
+      for (int bi = 0; bi < buckets.length; bi++) {
+        buckets[bi] = new LinkedList<>();
+      }
+    }
+
+    public void put(K key, V value) throws Exception {
+      // write your code here
+    }
+
+    public V get(K key) throws Exception {
+      // write your code here
+    }
+
+    public boolean containsKey(K key) {
+      // write your code here
+    }
+
+    public V remove(K key) throws Exception {
+      // write your code here
+    }
+
+    public ArrayList<K> keyset() throws Exception {
+      // write your code here
+    }
+
+    public int size() {
+      // write your code here
+    }
+
+   public void display() {
+      System.out.println("Display Begins");
+      for (int bi = 0; bi < buckets.length; bi++) {
+        System.out.print("Bucket" + bi + " ");
+        for (HMNode node : buckets[bi]) {
+          System.out.print( node.key + "@" + node.value + " ");
+        }
+        System.out.println(".");
+      }
+      System.out.println("Display Ends");
+  }
+}
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    HashMap<String, Integer> map = new HashMap();
+
+    String str = br.readLine();
+    while (str.equals("quit") == false) {
+      if (str.startsWith("put")) {
+        String[] parts = str.split(" ");
+        String key = parts[1];
+        Integer val = Integer.parseInt(parts[2]);
+        map.put(key, val);
+      } else if (str.startsWith("get")) {
+        String[] parts = str.split(" ");
+        String key = parts[1];
+        System.out.println(map.get(key));
+      } else if (str.startsWith("containsKey")) {
+        String[] parts = str.split(" ");
+        String key = parts[1];
+        System.out.println(map.containsKey(key));
+      } else if (str.startsWith("remove")) {
+        String[] parts = str.split(" ");
+        String key = parts[1];
+        System.out.println(map.remove(key));
+      } else if (str.startsWith("size")) {
+        System.out.println(map.size());
+      } else if (str.startsWith("keyset")) {
+        System.out.println(map.keyset());
+      } else if (str.startsWith("display")) {
+        map.display();
+      }
+      str = br.readLine();
+    }
+  }
+}
