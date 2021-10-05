5dc4302f0b959da2a3fa3f0f23db6b87f18e3f09
VisualTaggy <visualtaggy@gmail.com>
2021-10-05 20:56:09 +0530

    [Soln]: Print all paths using graph

--- 4.AdvancedDataStructure/Graphs/PrintAllPaths.java
+++ 4.AdvancedDataStructure/Graphs/PrintAllPaths.java
@@ -10,8 +10,64 @@
 
 //     The lexicographically smaller path is printed first.
 
-public class PrintAllPaths {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.*;
 
+public class PrintAllPaths {
+    static class Edge {
+        int src;
+        int nbr;
+        int wt;
+
+        Edge(int src, int nbr, int wt) {
+            this.src = src;
+            this.nbr = nbr;
+            this.wt = wt;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int vtces = Integer.parseInt(br.readLine());
+        ArrayList<Edge>[] graph = new ArrayList[vtces];
+        for (int i = 0; i < vtces; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        int edges = Integer.parseInt(br.readLine());
+        for (int i = 0; i < edges; i++) {
+            String[] parts = br.readLine().split(" ");
+            int v1 = Integer.parseInt(parts[0]);
+            int v2 = Integer.parseInt(parts[1]);
+            int wt = Integer.parseInt(parts[2]);
+            graph[v1].add(new Edge(v1, v2, wt));
+            graph[v2].add(new Edge(v2, v1, wt));
+        }
+
+        int src = Integer.parseInt(br.readLine());
+        int dest = Integer.parseInt(br.readLine());
+
+        // write all your codes here
+        boolean[] visited = new boolean[vtces];
+        print_all_paths(graph, src, dest, visited, src + "");
+    }
+
+    public static void print_all_paths(ArrayList<Edge>[] graph, int src, int dest, boolean[] visited, String path) {
+        if (src == dest) {
+            System.out.println(path);
+            return;
+        }
+
+        // setting current block to visited
+        visited[src] = true;
+
+        for (Edge edge : graph[src]) {
+            if (!visited[edge.nbr]) {
+                print_all_paths(graph, edge.nbr, dest, visited, path + edge.nbr);
+            }
+        }
+        // for other paths using same way(half of it)
+        visited[src] = false;
     }
 }
